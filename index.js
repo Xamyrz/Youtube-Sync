e305ce987b8802c8c92f19a0b70ef9be899b4da5
BuildTools <unconfigured@null.spigotmc.org>
2018-09-03 22:56:38 +0100

    status logs

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 const PORT = process.env.PORT || 5000;
-
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
-
+ 
 io.on('connection', function(socket){
+  socket.on('status', function(status){
+    console.log(status);
+  });
   console.log('a user connected');
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+    console.log('a user disconnected')
+  });
 });
-
+ 
+ 
+ 
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
