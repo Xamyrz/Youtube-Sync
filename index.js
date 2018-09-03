f5af9c7243717dd847e23c8fe8a0a57a7c95fc90
BuildTools <unconfigured@null.spigotmc.org>
2018-09-03 15:57:23 +0100

    master

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 var app = express();
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+})
